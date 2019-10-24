15e926c2627cf05d9c1f2999497b383ad9a8715c
Eric Wuerfel <Eric@Erics-MacBook-Pro-5.local>
2019-10-24 12:55:17 -0400

    changed imports to app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+# System Imports
 import os
+import requests
+import json 
 
 #Flask Imports 
 from flask import Flask, jsonify, request
 from flask_restful import Api, reqparse
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 # Local imports
 from .models import db, Modis
@@ -13,6 +17,11 @@ from .functions import (
     add_training_data, 
     haversine
 )
+
+# DS Logic imports
+import pandas as pd
+import numpy as np
+from math import radians, cos, sin, asin, sqrt
 
 
 def create_app():
